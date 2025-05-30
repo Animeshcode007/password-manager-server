36d56bec07db791cf8294d642e522b675fe20178
Animesh Khare <khareanimesh6@gmail.com>
2025-05-30 12:28:05 +0530

    server.js

--- server.js
+++ server.js
@@ -2,18 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-
+require("./db");
 const authRoutes = require("./routes/auth");
 const credentialRoutes = require("./routes/credentials");
 
 const app = express();
-app.use(
-  cors({
-    origin: process.env.VITE_API_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // only if you use cookies; omit if you don’t
-  })
-);
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
